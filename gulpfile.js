688e7b88e7c55ef27e105e906d6810c6bac2824b
kulebaev_konstantin <kkulebaev@yandex.ru>
2021-07-09 21:36:35 +0300

    optimazed some img

--- gulpfile.js
+++ gulpfile.js
@@ -23,239 +23,239 @@ const srcPath = 'src/'
 const distPath = 'dist/'
 
 const path = {
-    build: {
-        html: distPath,
-        js: distPath + 'assets/js/',
-        css: distPath + 'assets/css/',
-        images: distPath + 'assets/images/',
-        fonts: distPath + 'assets/fonts/',
-    },
-    src: {
-        html: srcPath + '*.html',
-        js: srcPath + 'assets/js/*.js',
-        css: srcPath + 'assets/scss/*.scss',
-        images: srcPath + 'assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}',
-        fonts: srcPath + 'assets/fonts/**/*.{eot,woff,woff2,ttf,svg}',
-    },
-    watch: {
-        html: srcPath + '**/*.html',
-        js: srcPath + 'assets/js/**/*.js',
-        css: srcPath + 'assets/scss/**/*.scss',
-        images: srcPath + 'assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}',
-        fonts: srcPath + 'assets/fonts/**/*.{eot,woff,woff2,ttf,svg}',
-    },
-    clean: './' + distPath,
+  build: {
+    html: distPath,
+    js: distPath + 'assets/js/',
+    css: distPath + 'assets/css/',
+    images: distPath + 'assets/images/',
+    fonts: distPath + 'assets/fonts/',
+  },
+  src: {
+    html: srcPath + '*.html',
+    js: srcPath + 'assets/js/*.js',
+    css: srcPath + 'assets/scss/*.scss',
+    images: srcPath + 'assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}',
+    fonts: srcPath + 'assets/fonts/**/*.{eot,woff,woff2,ttf,svg}',
+  },
+  watch: {
+    html: srcPath + '**/*.html',
+    js: srcPath + 'assets/js/**/*.js',
+    css: srcPath + 'assets/scss/**/*.scss',
+    images: srcPath + 'assets/images/**/*.{jpg,png,svg,gif,ico,webp,webmanifest,xml,json}',
+    fonts: srcPath + 'assets/fonts/**/*.{eot,woff,woff2,ttf,svg}',
+  },
+  clean: './' + distPath,
 }
 
 /* Tasks */
 
 function serve() {
-    browserSync.init({
-        server: {
-            baseDir: './' + distPath,
-        },
-    })
+  browserSync.init({
+    server: {
+      baseDir: './' + distPath,
+    },
+  })
 }
 
 function html(cb) {
-    panini.refresh()
-    return src(path.src.html, { base: srcPath })
-        .pipe(plumber())
-        .pipe(
-            panini({
-                root: srcPath,
-                layouts: srcPath + 'layouts/',
-                partials: srcPath + 'partials/',
-                helpers: srcPath + 'helpers/',
-                data: srcPath + 'data/',
-            })
-        )
-        .pipe(dest(path.build.html))
-        .pipe(browserSync.reload({ stream: true }))
+  panini.refresh()
+  return src(path.src.html, { base: srcPath })
+    .pipe(plumber())
+    .pipe(
+      panini({
+        root: srcPath,
+        layouts: srcPath + 'layouts/',
+        partials: srcPath + 'partials/',
+        helpers: srcPath + 'helpers/',
+        data: srcPath + 'data/',
+      })
+    )
+    .pipe(dest(path.build.html))
+    .pipe(browserSync.reload({ stream: true }))
 
-    cb()
+  cb()
 }
 
 function css(cb) {
-    return src(path.src.css, { base: srcPath + 'assets/scss/' })
-        .pipe(
-            plumber({
-                errorHandler: function (err) {
-                    notify.onError({
-                        title: 'SCSS Error',
-                        message: 'Error: <%= error.message %>',
-                    })(err)
-                    this.emit('end')
-                },
-            })
-        )
-        .pipe(
-            sass({
-                includePaths: './node_modules/',
-            })
-        )
-        .pipe(
-            autoprefixer({
-                cascade: true,
-            })
-        )
-        .pipe(cssbeautify())
-        .pipe(dest(path.build.css))
-        .pipe(
-            cssnano({
-                zindex: false,
-                discardComments: {
-                    removeAll: true,
-                },
-            })
-        )
-        .pipe(removeComments())
-        .pipe(
-            rename({
-                suffix: '.min',
-                extname: '.css',
-            })
-        )
-        .pipe(dest(path.build.css))
-        .pipe(browserSync.reload({ stream: true }))
+  return src(path.src.css, { base: srcPath + 'assets/scss/' })
+    .pipe(
+      plumber({
+        errorHandler: function (err) {
+          notify.onError({
+            title: 'SCSS Error',
+            message: 'Error: <%= error.message %>',
+          })(err)
+          this.emit('end')
+        },
+      })
+    )
+    .pipe(
+      sass({
+        includePaths: './node_modules/',
+      })
+    )
+    .pipe(
+      autoprefixer({
+        cascade: true,
+      })
+    )
+    .pipe(cssbeautify())
+    .pipe(dest(path.build.css))
+    .pipe(
+      cssnano({
+        zindex: false,
+        discardComments: {
+          removeAll: true,
+        },
+      })
+    )
+    .pipe(removeComments())
+    .pipe(
+      rename({
+        suffix: '.min',
+        extname: '.css',
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browserSync.reload({ stream: true }))
 
-    cb()
+  cb()
 }
 
 function cssWatch(cb) {
-    return src(path.src.css, { base: srcPath + 'assets/scss/' })
-        .pipe(
-            plumber({
-                errorHandler: function (err) {
-                    notify.onError({
-                        title: 'SCSS Error',
-                        message: 'Error: <%= error.message %>',
-                    })(err)
-                    this.emit('end')
-                },
-            })
-        )
-        .pipe(
-            sass({
-                includePaths: './node_modules/',
-            })
-        )
-        .pipe(
-            rename({
-                suffix: '.min',
-                extname: '.css',
-            })
-        )
-        .pipe(dest(path.build.css))
-        .pipe(browserSync.reload({ stream: true }))
+  return src(path.src.css, { base: srcPath + 'assets/scss/' })
+    .pipe(
+      plumber({
+        errorHandler: function (err) {
+          notify.onError({
+            title: 'SCSS Error',
+            message: 'Error: <%= error.message %>',
+          })(err)
+          this.emit('end')
+        },
+      })
+    )
+    .pipe(
+      sass({
+        includePaths: './node_modules/',
+      })
+    )
+    .pipe(
+      rename({
+        suffix: '.min',
+        extname: '.css',
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browserSync.reload({ stream: true }))
 
-    cb()
+  cb()
 }
 
 function js(cb) {
-    return src(path.src.js, { base: srcPath + 'assets/js/' })
-        .pipe(
-            plumber({
-                errorHandler: function (err) {
-                    notify.onError({
-                        title: 'JS Error',
-                        message: 'Error: <%= error.message %>',
-                    })(err)
-                    this.emit('end')
-                },
-            })
-        )
-        .pipe(
-            webpackStream({
-                mode: 'production',
-                output: {
-                    filename: 'app.js',
-                },
-                module: {
-                    rules: [
-                        {
-                            test: /\.(js)$/,
-                            exclude: /(node_modules)/,
-                            loader: 'babel-loader',
-                            query: {
-                                presets: ['@babel/preset-env'],
-                            },
-                        },
-                    ],
-                },
-            })
-        )
-        .pipe(dest(path.build.js))
-        .pipe(browserSync.reload({ stream: true }))
+  return src(path.src.js, { base: srcPath + 'assets/js/' })
+    .pipe(
+      plumber({
+        errorHandler: function (err) {
+          notify.onError({
+            title: 'JS Error',
+            message: 'Error: <%= error.message %>',
+          })(err)
+          this.emit('end')
+        },
+      })
+    )
+    .pipe(
+      webpackStream({
+        mode: 'production',
+        output: {
+          filename: 'app.js',
+        },
+        module: {
+          rules: [
+            {
+              test: /\.(js)$/,
+              exclude: /(node_modules)/,
+              loader: 'babel-loader',
+              query: {
+                presets: ['@babel/preset-env'],
+              },
+            },
+          ],
+        },
+      })
+    )
+    .pipe(dest(path.build.js))
+    .pipe(browserSync.reload({ stream: true }))
 
-    cb()
+  cb()
 }
 
 function jsWatch(cb) {
-    return src(path.src.js, { base: srcPath + 'assets/js/' })
-        .pipe(
-            plumber({
-                errorHandler: function (err) {
-                    notify.onError({
-                        title: 'JS Error',
-                        message: 'Error: <%= error.message %>',
-                    })(err)
-                    this.emit('end')
-                },
-            })
-        )
-        .pipe(
-            webpackStream({
-                mode: 'development',
-                output: {
-                    filename: 'app.js',
-                },
-            })
-        )
-        .pipe(dest(path.build.js))
-        .pipe(browserSync.reload({ stream: true }))
+  return src(path.src.js, { base: srcPath + 'assets/js/' })
+    .pipe(
+      plumber({
+        errorHandler: function (err) {
+          notify.onError({
+            title: 'JS Error',
+            message: 'Error: <%= error.message %>',
+          })(err)
+          this.emit('end')
+        },
+      })
+    )
+    .pipe(
+      webpackStream({
+        mode: 'development',
+        output: {
+          filename: 'app.js',
+        },
+      })
+    )
+    .pipe(dest(path.build.js))
+    .pipe(browserSync.reload({ stream: true }))
 
-    cb()
+  cb()
 }
 
 function images(cb) {
-    return src(path.src.images)
-        .pipe(
-            imagemin([
-                imagemin.gifsicle({ interlaced: true }),
-                imagemin.mozjpeg({ quality: 95, progressive: true }),
-                imagemin.optipng({ optimizationLevel: 5 }),
-                imagemin.svgo({
-                    plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
-                }),
-            ])
-        )
-        .pipe(dest(path.build.images))
-        .pipe(browserSync.reload({ stream: true }))
+  return src(path.src.images)
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.mozjpeg({ quality: 95, progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+        }),
+      ])
+    )
+    .pipe(dest(path.build.images))
+    .pipe(browserSync.reload({ stream: true }))
 
-    cb()
+  cb()
 }
 
 function fonts(cb) {
-    return src(path.src.fonts)
-        .pipe(dest(path.build.fonts))
-        .pipe(browserSync.reload({ stream: true }))
+  return src(path.src.fonts)
+    .pipe(dest(path.build.fonts))
+    .pipe(browserSync.reload({ stream: true }))
 
-    cb()
+  cb()
 }
 
 function clean(cb) {
-    return del(path.clean)
+  return del(path.clean)
 
-    cb()
+  cb()
 }
 
 function watchFiles() {
-    gulp.watch([path.watch.html], html)
-    gulp.watch([path.watch.css], cssWatch)
-    gulp.watch([path.watch.js], jsWatch)
-    gulp.watch([path.watch.images], images)
-    gulp.watch([path.watch.fonts], fonts)
+  gulp.watch([path.watch.html], html)
+  gulp.watch([path.watch.css], cssWatch)
+  gulp.watch([path.watch.js], jsWatch)
+  gulp.watch([path.watch.images], images)
+  gulp.watch([path.watch.fonts], fonts)
 }
 
 const build = gulp.series(clean, gulp.parallel(html, css, js, images, fonts))
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,91 +1,91 @@
 //= ../../../node_modules/jquery-parallax.js/parallax.js
 
 $(function () {
-    //= components/parallax.js
+  //= components/parallax.js
 
-    /* Parallax 
+  /* Parallax 
     ================*/
-    $('#parallax').parallax({ imageSrc: 'assets/images/parallax.jpg' })
-    $('#intro-parallax').parallax({ imageSrc: 'assets/images/intro.jpg' })
+  $('#parallax').parallax({ imageSrc: 'assets/images/parallax.jpg' })
+  $('#intro-parallax').parallax({ imageSrc: 'assets/images/intro.jpg' })
 
-    /* Fixed Header 
+  /* Fixed Header 
     ================*/
-    let header = $('#header')
-    let intro = $('#intro')
+  let header = $('#header')
+  let intro = $('#intro')
+  let introH = intro.innerHeight()
+  let scrollPos = $(window).scrollTop()
+
+  checkScroll(scrollPos, introH)
+
+  $(window).on('scroll resize', function () {
     let introH = intro.innerHeight()
-    let scrollPos = $(window).scrollTop()
+    scrollPos = $(this).scrollTop()
 
     checkScroll(scrollPos, introH)
+  })
 
-    $(window).on('scroll resize', function () {
-        let introH = intro.innerHeight()
-        scrollPos = $(this).scrollTop()
-
-        checkScroll(scrollPos, introH)
-    })
-
-    function checkScroll(scrollPos, introH) {
-        if (scrollPos > introH / 2) {
-            header.addClass('fixed')
-        } else {
-            header.removeClass('fixed')
-        }
+  function checkScroll(scrollPos, introH) {
+    if (scrollPos > introH / 2) {
+      header.addClass('fixed')
+    } else {
+      header.removeClass('fixed')
     }
+  }
 
-    /* Smooth scroll 
+  /* Smooth scroll 
     ==================*/
-    $('[data-scroll]').on('click', function (event) {
-        event.preventDefault()
+  $('[data-scroll]').on('click', function (event) {
+    event.preventDefault()
 
-        let elementId = $(this).data('scroll')
-        let elementOffset = $(elementId).offset().top
+    let elementId = $(this).data('scroll')
+    let elementOffset = $(elementId).offset().top
 
-        $('html, body').animate(
-            {
-                scrollTop: elementOffset,
-            },
-            1000
-        )
+    $('html, body').animate(
+      {
+        scrollTop: elementOffset,
+      },
+      1000
+    )
 
-        // $(burger).toggleClass('burger--active')
-        // $(nav).toggleClass('nav--active')
+    // $(burger).toggleClass('burger--active')
+    // $(nav).toggleClass('nav--active')
+  })
+
+  /* Burger menu 
+    ========================*/
+  // let nav = document.querySelector('#nav')
+  // let burger = document.querySelector('#burger')
+
+  // $(burger).on('click', function (event) {
+  //     event.preventDefault()
+  //     $(burger).toggleClass('burger--active')
+  //     $(nav).toggleClass('nav--active')
+  // })
+
+  /* Filter
+    ========================*/
+  let filter = $('[data-filter]')
+  filter.on('click', function (event) {
+    event.preventDefault()
+
+    let cat = $(this).data('filter')
+    filter.each(function () {
+      $(this).removeClass('category__item--active')
     })
+    $(this).addClass('category__item--active')
 
-    /* Burger menu 
-    ========================*/
-    // let nav = document.querySelector('#nav')
-    // let burger = document.querySelector('#burger')
+    if (cat == 'all') {
+      $('[data-cat]').removeClass('cards__item--hidden')
+    } else {
+      $('[data-cat]').each(function () {
+        let itemCat = $(this).data('cat')
 
-    // $(burger).on('click', function (event) {
-    //     event.preventDefault()
-    //     $(burger).toggleClass('burger--active')
-    //     $(nav).toggleClass('nav--active')
-    // })
-
-    /* Filter
-    ========================*/
-    let filter = $('[data-filter]')
-    filter.on('click', function (event) {
-        event.preventDefault()
-
-        let cat = $(this).data('filter')
-        filter.each(function () {
-            $(this).removeClass('category__item--active')
-        })
-        $(this).addClass('category__item--active')
-
-        if (cat == 'all') {
-            $('[data-cat]').removeClass('cards__item--hidden')
+        if (itemCat != cat) {
+          $(this).addClass('cards__item--hidden')
         } else {
-            $('[data-cat]').each(function () {
-                let itemCat = $(this).data('cat')
-
-                if (itemCat != cat) {
-                    $(this).addClass('cards__item--hidden')
-                } else {
-                    $(this).removeClass('cards__item--hidden')
-                }
-            })
+          $(this).removeClass('cards__item--hidden')
         }
-    })
+      })
+    }
+  })
 })
